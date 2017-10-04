7c58f1b42e62f9f020f4ac7d286f8fc38eb7a208
unisrn0110 <unisrn0110@gmail.com>
2017-10-05 00:17:51 +0700

    case 1 - 3

--- BasicConsoleApplication/Program.cs
+++ BasicConsoleApplication/Program.cs
@@ -10,41 +10,54 @@ namespace BasicConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------- MENU ---------------");
-            Console.WriteLine("1. Hello World");
-            Console.WriteLine("2. Hello Name");
-            Console.WriteLine("3. Calculator");
-            Console.WriteLine("4. Even or Odd Number");
-            Console.WriteLine("5. Multiplication");
-            Console.WriteLine("------------------------------------");
-            Console.Write("Input your choice: ");
-            string choice = Console.ReadLine();
-            Console.WriteLine("------------------------------------");
+            string choice = "";
 
-            switch (choice)
+            do
             {
-                case "1":
-                    Console.WriteLine("Hello World!!!");
+                Console.WriteLine("------------------- MENU -------------------");
+                Console.WriteLine("1. Hello World");
+                Console.WriteLine("2. Hello Name");
+                Console.WriteLine("3. Calculator");
+                Console.WriteLine("4. Even or Odd Number");
+                Console.WriteLine("5. Multiplication");
+                Console.WriteLine("--------------------------------------------");
+                Console.Write("Input your choice: ");
+                choice = Console.ReadLine();
+                Console.WriteLine("--------------------------------------------");
+
+                if (choice == "0")
+                {
+                    Console.WriteLine("End Program");
+                    Console.WriteLine("--------------------------------------------");
                     break;
-                case "2":
-                    Console.Write("Please input your name: ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("\n>>> Hello " + name);
-                    break;
-                case "3":
-                    Console.WriteLine("1. Add | 2. Minus | 3. Multiple | 4. Divide");
-                    Console.Write("Select operator (number 1 - 4): ");
-                    string operation = Console.ReadLine();
-                    Console.Write("Input number1: ");
-                    int num1 = Int32.Parse(Console.ReadLine());
-                    Console.Write("Input number1: ");
-                    int num2 = Int32.Parse(Console.ReadLine());
-                    calculator(operation, num1, num2);
-                    break;
-                default:
-                    Console.WriteLine("Please try again");
-                    break;
-            }
+                }
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Hello World!!!");
+                        break;
+                    case "2":
+                        Console.Write("Please input your name: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("\n>>> Hello " + name);
+                        break;
+                    case "3":
+                        Console.WriteLine("1. Add | 2. Minus | 3. Multiple | 4. Divide");
+                        Console.Write("Select operator (number 1 - 4): ");
+                        string operation = Console.ReadLine();
+                        Console.Write("Input number1: ");
+                        int num1 = Int32.Parse(Console.ReadLine());
+                        Console.Write("Input number1: ");
+                        int num2 = Int32.Parse(Console.ReadLine());
+                        calculator(operation, num1, num2);
+                        break;
+                    default:
+                        Console.WriteLine("Please try again");
+                        break;
+                }
+            } while (choice != "0");
+            
 
             Console.ReadKey();
         }
@@ -55,19 +68,19 @@ namespace BasicConsoleApplication
             {
                 case "1":
                     int resAdd = num1 + num2;
-                    Console.WriteLine(num1 + " + " + num2 + " = " + resAdd);
+                    Console.WriteLine("\n>>> " + num1 + " + " + num2 + " = " + resAdd);
                     break;
                 case "2":
                     int resMinus = num1 - num2;
-                    Console.WriteLine(num1 + " - " + num2 + " = " + resMinus);
+                    Console.WriteLine("\n>>> " + num1 + " - " + num2 + " = " + resMinus);
                     break;
                 case "3":
                     int resMuliple = num1 * num2;
-                    Console.WriteLine(num1 + " x " + num2 + " = " + resMuliple);
+                    Console.WriteLine("\n>>> " + num1 + " x " + num2 + " = " + resMuliple);
                     break;
                 case "4":
                     int resDivide = num1 / num2;
-                    Console.WriteLine(num1 + " / " + num2 + " = " + resDivide);
+                    Console.WriteLine("\n>>> " + num1 + " / " + num2 + " = " + resDivide);
                     break;
                 default:
                     Console.WriteLine("Invalid Case");
